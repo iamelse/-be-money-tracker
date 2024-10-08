6679225569763005129218908781ec721f7d5ab9
Lana Septiana <lana.septiana2@gmail.com>
2024-10-08 14:21:17 +0700

    feat(signin page): implement signin page * setting up alpine js * setting up project template TailAdmin

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>{{ $title ?? env('APP_NAME') }}</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+  </head>
+
+  <body
+    x-data="{ page: 'signin', 'loaded': true, 'darkMode': true, 'stickyMenu': false, 'sidebarToggle': false, 'scrollTop': false }"
+    x-init="
+          darkMode = JSON.parse(localStorage.getItem('darkMode'));
+          $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))"
+    :class="{'dark text-bodydark bg-boxdark-2': darkMode === true}"
+  >
+    <!-- ===== Preloader Start ===== -->
+    @include('partials.preloader')
+    <!-- ===== Preloader End ===== -->
+
+    <!-- ===== Page Wrapper Start ===== -->
+    <div class="flex h-screen overflow-hidden">
+
+      @yield('content')
+    </div>
+    <!-- ===== Page Wrapper End ===== -->
+  </body>
+</html>
