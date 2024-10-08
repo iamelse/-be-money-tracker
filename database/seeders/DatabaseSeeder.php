2ad321fe6740bb37daf4858ccfbc9ddbb1a4c23a
Lana Septiana <lana.septiana2@gmail.com>
2024-10-08 16:31:46 +0700

    feat(user seeder): single row seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,8 +8,11 @@ use App\Models\Goal;
 use App\Models\Report;
 use App\Models\Transaction;
 use App\Models\User;
+use Carbon\Carbon;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,6 +22,16 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+
+        DB::table('users')->insert([
+            'name' => 'Lana Septiana',
+            'email' => 'lana.septiana2@gmail.com',
+            'email_verified_at' => Carbon::now(),
+            'password' => bcrypt('password'),
+            'remember_token' => Str::random(10),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
 
         User::factory(10)->create()->each(function ($user) {
             $user->accounts()->saveMany(Account::factory(3)->make());
